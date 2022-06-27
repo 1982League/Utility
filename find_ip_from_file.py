b3c18c43262d377e052f289b3edc3371a436c6aa
adminSecurity1982 <siddharth.joshi@mycit.ie>
2022-06-27 17:47:01 +0100

    files added

---
+++ find_ip_from_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys, re
+
+f = open(sys.argv[1],'r')
+ip = input("IP: ")
+text = f.read()
+ips = []
+regex = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b',text)
+if regex is not None:
+    for match in regex:
+        if match not in ips:
+            ips.append(match)
+            if ip in match:
+                print(f"{ip} is in the file")
+                break
+        else:
+            print(f"{ip} not found")
+            break
+
+#============================= Sample Script Output =============================#
+#./find_ip1.py ip.txt
+#IP: 10.1.81.1
+#10.1.81.1 is in the file
+#================================================================================#
